69ded15dd67bf2fd34985ddbbccd5c345646926d
Jakob Huber <93046@psdschools.org>
2022-09-01 09:20:12 -0600

    MVP

--- UiTextFieldsHackWich/ViewController.swift
+++ UiTextFieldsHackWich/ViewController.swift
@@ -8,12 +8,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
+    
+    
 
+//    @IBAction func nameTextField(_ sender: Any) {}
+    
+         
+   
+    @IBOutlet var nametTextField: UITextField!
+  
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
         // Do any additional setup after loading the view.
+ 
+    
+    
+    
+    
+    }
+  
+    @IBAction func actionButtonTapped(_ sender: Any) {   nameLabel.text = nametTextField.text
+        nametTextField.resignFirstResponder()
+        
+        
+    }
+    
+    
+    
     }
 
 
-}
+    
+    
+    
+    
+  
+            
+            
+         
+            
+            
+            
+    
+    
+    
+    
+    
+
+  
+    
+    
+    
+    
+    
+    
+    
+ 
+    
+    
+    
+
 
